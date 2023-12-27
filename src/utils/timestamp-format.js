aad3ff497cf296822e83d23846eb99123aa36aaa
jerome greder <j.greder@staro.ch>
2023-12-27 17:40:08 +0100

    Documentation added for formatDate function

--- src/utils/timestamp-format.js
+++ src/utils/timestamp-format.js
@@ -1,3 +1,9 @@
+/**
+ * formatDate is a utility function that takes a timestamp and returns a string representation of the date in 'en-GB' format.
+ *
+ * @param {number} timestamp - The timestamp to be converted into a date string.
+ * @returns {string} The date string in 'en-GB' format.
+ */
 function formatDate(timestamp) {
     const date = new Date(timestamp);
     return date.toLocaleDateString('en-GB');
