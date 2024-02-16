3073b3573b7785305d5b3b0f2e381e90ad7163ee
jerome greder <j.greder@staro.ch>
2024-02-16 11:07:00 +0100

    Refactor MetaDataReader and rename formatDate function
    The MetaDataReader function in `metaDataReader.js` has been refactored to streamline the return of EXIF data and improve error handling. Additionally, the `formatDate` function in `timeStampFormat.js` has been renamed to `FormatDate` for consistency.

--- src/utils/metaDataReader.js
+++ src/utils/metaDataReader.js
@@ -1,14 +1,20 @@
 const ExifReader = require('exifreader');
 
+/**
+ * MetaDataReader function to read EXIF data from an image URL.
+ * @async
+ * @param {string} imageUrl - The URL of the image to read EXIF data from.
+ * @returns {Promise<object>} A promise that resolves to an object containing the EXIF data.
+ * @throws Will throw an error if the fetch operation fails.
+ */
 export default async function MetaDataReader(imageUrl) {
     try {
         const response = await fetch(imageUrl);
         const blob = await response.blob();
         const arrayBuffer = await new Response(blob).arrayBuffer();
-        const tags = ExifReader.load(arrayBuffer);
-        // Process and return the EXIF data as needed
-        return tags;
+        return ExifReader.load(arrayBuffer);
     } catch (error) {
+        // Log the error and return an empty object
         console.error('Error loading EXIF data:', error);
         return {}; // Return an empty object or handle the error as needed
     }
