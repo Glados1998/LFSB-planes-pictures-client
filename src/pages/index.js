fa58c38c1040b883dea4939f494c06fc3dc66071
jerome greder <j.greder@staro.ch>
2024-05-22 23:12:14 +0200

    Add multilingual support and update homepage content
    This commit integrates multilingual support into the project by using the 'next-intl' library. In parallel, the homepage content has been updated and translated to English, German, and French to fit into the multilingual setup. This action also involves extracting translations into json files and changing hard-coded text in the application to use the standard library function for translations.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,23 +15,22 @@ export async function getStaticProps(context) {
 
 export default function Home() {
 
-    const t = useTranslations();
+    const t = useTranslations("home");
     return (
         <div className={'home'}>
             <div className="home_headline">
                 <div className="home_headline-text">
                     <h2>
-                        {t('home.headline')}
+                        {t('headline')}
                     </h2>
                     <h1 className={'title'}>
                         LFSB Planes Pictures
                     </h1>
                     <hr/>
                     <p>
-                        Entrez dans un monde où le rugissement des moteurs à réaction et l'élégance du design des avions
-                        prennent vie. Bienvenue dans ma galerie photo privée, capturant l'esprit dynamique de l'aviation
-                        à <span>l'EuroAirport Bâle-Mulhouse-Fribourg</span> –
-                        un carrefour des cultures et de l'innovation aéronautique.
+                        {t.rich('subheadline', {
+                            span: (chunks) => <span>{chunks}</span>,
+                        })}
                     </p>
                 </div>
 
@@ -41,19 +40,14 @@ export default function Home() {
                     <Image src={papaHeadshot} alt={'Laurent Greder'} width={650} height={500} objectFit={'cover'}/>
                 </div>
                 <div className={'text'}>
-                    <h1>À propos du photographe</h1>
+                    <h1>{t('about.headline')}</h1>
                     <p>
-                        Salutations ! Je suis Laurent Greder, un passionné d'aviation et spotter dévoué. Mon parcours
-                        dans la photographie aéronautique a commencé il y a plusieurs années, alimenté par une
-                        fascination profonde pour ces merveilles d'ingénierie et les histoires qu'elles portent à
-                        travers les cieux.
+                        {t("about.text1")}
                     </p>
                     <p>
-                        Bien que mon principal terrain de jeu soit l'effervescent <span>EuroAirport Bâle-Mulhouse-Fribourg</span>,
-                        il m'arrive occasionnellement de capturer des scènes d'aviation uniques à
-                        <span>l'aéroport de Genève (LSGG)</span> and <span>l'aéroport de Zurich (LSZH)</span>.
-                        Chaque lieu offre un aperçu distinct du monde de l'aviation, des hubs internationaux animés aux
-                        départs sereins vers le ciel.
+                        {t.rich('about.text2', {
+                            span: (chunks) => <span>{chunks}</span>,
+                        })}
                     </p>
                 </div>
             </div>
