17cfba0af11a14dffbe6d55bfb7db4a23edc4624
jerome greder <j.greder@staro.ch>
2026-01-19 01:56:17 +0100

    Add getStaticProps to 404 and 500 pages for localized messages

--- src/pages/400.js
+++ src/pages/400.js
@@ -1,3 +1,11 @@
+export async function getStaticProps({locale}) {
+    return {
+        props: {
+            messages: (await import(`../messages/${locale}.json`)).default
+        }
+    };
+}
+
 export default function Custom404() {
     return <h1>404 - Page Not Found</h1>
 }
--- src/pages/500.js
+++ src/pages/500.js
@@ -1,3 +1,11 @@
+export async function getStaticProps({locale}) {
+    return {
+        props: {
+            messages: (await import(`../messages/${locale}.json`)).default
+        }
+    };
+}
+
 export default function Custom500() {
     return <h1>500 - Server-side error occurred</h1>
 }
