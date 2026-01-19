a92097b67c086c4dbd035043eeecae0dde9dd365
jerome greder <j.greder@staro.ch>
2026-01-19 11:35:16 +0100

    Add internationalization support for 404 and 500 error pages

--- src/pages/400.js
+++ src/pages/400.js
@@ -1,3 +1,18 @@
+import {useTranslations} from "next-intl";
+
+export async function getStaticProps(context) {
+    return {
+        props: {
+            // You can get the messages from anywhere you like. The recommended
+            // pattern is to put them in JSON files separated by locale and read
+            // the desired one based on the `locale` received from Next.js.
+            messages: (await import(`public/locales/${context.locale}.json`)).default
+        }
+    };
+}
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>
+
+    const t = useTranslations("specialPages");
+    return <h1>{t("404")}</h1>
 }
--- src/pages/500.js
+++ src/pages/500.js
@@ -1,3 +1,18 @@
+import {useTranslations} from "next-intl";
+
+export async function getStaticProps(context) {
+    return {
+        props: {
+            // You can get the messages from anywhere you like. The recommended
+            // pattern is to put them in JSON files separated by locale and read
+            // the desired one based on the `locale` received from Next.js.
+            messages: (await import(`public/locales/${context.locale}.json`)).default
+        }
+    };
+}
+
 export default function Custom500() {
-    return <h1>500 - Server-side error occurred</h1>
+
+    const t = useTranslations("specialPages");
+    return <h1>{t("505")}</h1>
 }
